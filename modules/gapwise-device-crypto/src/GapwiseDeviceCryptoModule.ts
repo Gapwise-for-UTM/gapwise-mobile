14b5945146e3a936adcd022439d2af208d253631
Andrew Muratov <andrewamuratov@gmail.com>
2026-08-28 01:04:02 +0200

    style: format native crypto bridge

--- modules/gapwise-device-crypto/src/GapwiseDeviceCryptoModule.ts
+++ modules/gapwise-device-crypto/src/GapwiseDeviceCryptoModule.ts
@@ -6,8 +6,9 @@ import type {
   GapwiseDeviceCryptoNativeModule,
 } from "./GapwiseDeviceCrypto.types";
 
-const native =
-  requireNativeModule<GapwiseDeviceCryptoNativeModule>("GapwiseDeviceCrypto");
+const native = requireNativeModule<GapwiseDeviceCryptoNativeModule>(
+  "GapwiseDeviceCrypto",
+);
 
 export function getOrCreatePublicJwk(accountId: string): DevicePublicJwk {
   return JSON.parse(native.getOrCreatePublicJwk(accountId)) as DevicePublicJwk;
